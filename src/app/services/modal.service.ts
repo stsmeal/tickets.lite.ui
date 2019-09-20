8a3d9f6424d39fda2b4d8a444b6ed2250b56ee9c
Sam Smeal <stsmeal@gmail.com>
2019-09-20 09:59:34 -0400

    Ticket Note Portal Change

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,20 +1,15 @@
 import { Injectable, TemplateRef } from "@angular/core";
 import { MatDialog, MatDialogRef, MatDialogConfig } from '@angular/material/dialog';
 import { ComponentType } from '@angular/cdk/overlay/index';
-import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ModalService {
     constructor(
-        public modal: MatDialog,
-        private breakpointObserver: BreakpointObserver){ }
+        public modal: MatDialog){ }
 
     public open<T>(componet: ComponentType<T> | TemplateRef<T>, config?: MatDialogConfig): MatDialogRef<T, any> {
-        if(!config){
-
-        }
         return this.modal.open(componet, config);
     }
 }
