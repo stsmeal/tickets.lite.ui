49ac2a1fd935adfa9a85c7e3dca0dcd7356856ef
Sam Smeal <stsmeal@gmail.com>
2019-09-23 17:47:07 -0400

    Favicon change & notification on unsuccessful login

--- src/app/shared/sign-in/sign-in.component.ts
+++ src/app/shared/sign-in/sign-in.component.ts
@@ -3,6 +3,7 @@ import { FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user';
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
     selector: 'sign-in',
@@ -18,7 +19,8 @@ export class SignInComponent {
 
     constructor(
         private auth: AuthService,
-        private router: Router) {}
+        private router: Router,
+        private notification: NotificationService) {}
 
     public signIn(): void {
         this.authenticating = true;
@@ -32,6 +34,12 @@ export class SignInComponent {
                 this.authenticating = false;
                 this.router.navigate(['/tickets']);
             }, (error) => {
+                if(error.error == 'Incorrect Username or Password'){
+                    this.notification.error(error.error);//<-iconic
+                } else {
+                    this.notification.error('An occurred while signing in');
+                }
+                console.log(error);
                 this.authenticating = false;
                 this.signInText = "Sign In";
             }
